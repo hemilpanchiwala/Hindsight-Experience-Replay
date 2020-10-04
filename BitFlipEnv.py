50b745bd8fdbcdd78f0ed56ffd0401c8d2032bfa
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2020-10-05 01:12:55 +0530

    add bit-flip environment

---
+++ BitFlipEnv.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+class BitFlipEnv:
+    """
+    A simple bit flip environment
+    Bit of the current state flips as an action
+    Reward of -1 for each step
+    """
+    def __init__(self, n_bits):
+        self.n_bits = n_bits
+        self.reward = -1
+        self.state = np.random.randint(2, size=self.n_bits)
+        self.goal = np.random.randint(2, size=self.n_bits)
+
+    def reset_env(self):
+        """
+        Resets the environment with new state and goal
+        """
+        self.state = np.random.randint(2, size=self.n_bits)
+        self.goal = np.random.randint(2, size=self.n_bits)
+
+    def take_step(self, action):
+        """
+        Returns updated_state, reward, and done for the step taken
+        """
+        self.state[action] = self.state[action] ^ 1
+        done = False
+        if self.state == self.goal:
+            done = True
+            reward = 0
+        else:
+            reward = -1
+        return self.state, reward, done
+
+    def print_state(self):
+        """
+        Prints the current state
+        """
+        print('Current State:', self.state)
