8abc6829f39ed1924b68916cd4cde9783573a9bf
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2020-10-07 16:31:13 +0530

    add actor-critic networks | add OUNoise from OpenAI baselines

---
+++ ActorCritic.py
@@ -0,0 +1,74 @@
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class Actor(nn.Module):
+    def __init__(self, input_dims, n_actions, learning_rate, checkpoint_dir, name):
+        super(Actor, self).__init__()
+
+        self.fc1 = nn.Linear(input_dims, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, n_actions)
+
+        self.optimizer = optim.RMSprop(self.parameters(), lr=learning_rate)
+        self.loss = nn.MSELoss()
+
+        self.device_type = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        self.device = torch.device(self.device_type)
+        self.to(self.device)
+
+        self.checkpoint_dir = checkpoint_dir
+        self.checkpoint_name = os.path.join(checkpoint_dir, name)
+
+    def forward(self, data):
+        fc_layer1 = F.relu(self.fc1(data))
+        fc_layer2 = F.relu(self.fc2(fc_layer1))
+        actions = self.fc3(fc_layer2)
+
+        return actions
+
+    def save_checkpoint(self):
+        print('Saving checkpoint ...')
+        torch.save(self.state_dict(), self.checkpoint_name)
+
+    def load_checkpoint(self):
+        print('Loading checkpoint ...')
+        self.load_state_dict(torch.load(self.checkpoint_name))
+
+
+class Critic(nn.Module):
+    def __init__(self, input_dims, n_actions, learning_rate, checkpoint_dir, name):
+        super(Critic, self).__init__()
+
+        self.fc1 = nn.Linear(input_dims + n_actions, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 1)
+
+        self.optimizer = optim.RMSprop(self.parameters(), lr=learning_rate)
+        self.loss = nn.MSELoss()
+
+        self.device_type = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        self.device = torch.device(self.device_type)
+        self.to(self.device)
+
+        self.checkpoint_dir = checkpoint_dir
+        self.checkpoint_name = os.path.join(checkpoint_dir, name)
+
+    def forward(self, data1, data2):
+        fc_layer1 = F.relu(self.fc1(torch.cat((data1, data2), 1)))
+        fc_layer2 = F.relu(self.fc2(fc_layer1))
+        actions = self.fc3(fc_layer2)
+
+        return actions
+
+    def save_checkpoint(self):
+        print('Saving checkpoint ...')
+        torch.save(self.state_dict(), self.checkpoint_name)
+
+    def load_checkpoint(self):
+        print('Loading checkpoint ...')
+        self.load_state_dict(torch.load(self.checkpoint_name))
--- DDPG.py
+++ DDPG.py
@@ -1,8 +1,36 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
+import numpy as np
+
+from ActorCritic import Actor, Critic
+from dqn_with_her import HERMemory as her
+import OUNoise as noise
 
 
-class DDPG(nn.Module):
-    def __init__(self):
+class DDPG:
+    def __init__(self, learning_rate, n_actions, input_dims,
+                 memory_size, batch_size, checkpoint_dir='/tmp/ddqn/'):
+        self.learning_rate = learning_rate
+        self.n_actions = n_actions
+        self.input_dims = input_dims
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+
+        self.actor = Actor(input_dims=input_dims, n_actions=n_actions,
+                           learning_rate=learning_rate, checkpoint_dir=checkpoint_dir,
+                           name='actor')
+
+        self.critic = Critic(input_dims=input_dims, n_actions=n_actions,
+                             learning_rate=learning_rate, checkpoint_dir=checkpoint_dir,
+                             name='critic')
+
+        self.target_actor = Actor(input_dims=input_dims, n_actions=n_actions,
+                                  learning_rate=learning_rate, checkpoint_dir=checkpoint_dir,
+                                  name='actor')
+
+        self.target_critic = Critic(input_dims=input_dims, n_actions=n_actions,
+                                    learning_rate=learning_rate, checkpoint_dir=checkpoint_dir,
+                                    name='critic')
+
+        self.memory = her.HindsightExperienceReplayMemory(memory_size=memory_size,
+                                                          input_dims=input_dims)
+
+        self.ou_noise = noise.OrnsteinUhlenbeckActionNoise(mu=np.zeros(n_actions))
